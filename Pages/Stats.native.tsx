f0fb419b3672bfb34589dfdabe4eee5e3b972739
Samad <eeysaad@nottingham.ac.uk>
2022-05-02 18:32:48 +0100

    Finished stats page

--- Pages/Stats.native.tsx
+++ Pages/Stats.native.tsx
@@ -1,40 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-    FlatList
-  } from 'react-native';
+  Text,
+  View,
+  FlatList
+} from 'react-native';
 import { grassData } from '../Services/myMowingService';
 import { stat } from '../ts/types';
+import { Button, Card } from '@rneui/themed';
 
 const Stats: React.FC<{
 }> = () => {
-    const data:stat[] = grassData
+  const data: stat[] = grassData
   return (
     <View>
-        
-        <FlatList    
-          style={{width:"100%"}}     
-          data={data}
-          keyExtractor= {(item) => {
-            return item.grassType;
-          }}
-          renderItem={({item}) => {
-            return (
-              
-                <View >
-                  <Text>This would be 1 of them</Text>
-                </View>
-              
-            )
-          }}
-          ListHeaderComponent={()=><Text>Grass Data</Text>}
-          />
+
+      <FlatList
+        style={{ width: "100%" }}
+        data={data}
+        keyExtractor={(item) => {
+          return item.grassType;
+        }}
+        renderItem={({ item }) => {
+          return (
+            <Card>
+              <Text style={{fontWeight:"bold"}}>{item.grassType}</Text>
+              <Text >Growth Rate Summer (cm/day): {item.growthRateSummer}</Text>
+              <Text>Growth Rate Winter (cm/day):{item.growthRateWinter}</Text>
+              <Text >Base length (cm): {item.bestLength}</Text>
+            </Card>
+          )
+        }}
+
+      />
     </View>
 
   );
